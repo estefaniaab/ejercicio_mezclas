111bc8b4876f570d6dbfe7b2152e5bd946b74817
DavidRodriguezCardona <davidrocar1311@gmail.com>
2025-02-14 20:38:22 -0500

    resta

--- chat.py
+++ chat.py
@@ -11,7 +11,13 @@ def multiplicacion(c,d):
     return c * d
 
 result=multiplicacion(5,10)
-print("El resultado es:",result)
+print("El resultado de la multiplicacion es:",result)
+
+def resta(a,b):
+    return a * b
+
+result=resta(4,20)
+print("El resultado de la resta es:",result)
 
 print ("oe")
 print("Funcionaaaaaaaa")
@@ -20,3 +26,5 @@ print ("Esta es mi ultima mezcla")
 print(3-1)
 
 print("szs")
+
+
